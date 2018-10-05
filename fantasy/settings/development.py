df29b72b6ac0ec481458b2399bfc88172d0baf6f
Thomas Kunwar <yathomasi@gmail.com>
2018-10-05 17:39:54 +0545

    Database placed in dev

--- fantasy/settings/development.py
+++ fantasy/settings/development.py
@@ -6,6 +6,20 @@ INSTALLED_APPS += [
     'debug_toolbar',
 ]
 
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASS'),
+        'HOST': config('DB_HOST'),
+        'PORT': '5432',
+    }
+}
+
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
